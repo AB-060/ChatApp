749204635193e812f3bb0327fe236f2c480a835f
ab060 <ab060@abdallahi.ab>
2026-05-28 14:00:39 +0000

    add icon

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/abdallahi/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/abdallahi/projects/messanger"
+export "FLUTTER_ROOT=/home/ab060/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/var/www/html/ChatApp"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/var/www/html/ChatApp/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
